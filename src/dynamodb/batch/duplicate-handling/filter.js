fa114af42f0f591aceee4bf252ed757d9175bb0d
Jens-Peter Olsson <jens-peter.olsson@it-budet.se>
2022-05-09 11:36:55 +0200

    feat: filter unique objects by timestamp attribute

--- src/dynamodb/batch/duplicate-handling/filter.js
+++ src/dynamodb/batch/duplicate-handling/filter.js
@@ -18,6 +18,64 @@ function filterUniqueKeys(keys = []) {
   return uniqueKeys;
 }
 
+const defaultOptions = {
+  duplicateConfig: {
+    partitionKeyAttributeName: '',
+    sortKeyAttributeName: '',
+    timestampAttributeName: ''
+  }
+};
+
+function keyComparer(object1, object2, options) {
+  const pk = options.duplicateConfig.partitionKeyAttributeName;
+  const sk = options.duplicateConfig.sortKeyAttributeName;
+
+  return object1[pk] === object2[pk] && object1[sk] === object2[sk];
+}
+
+function objectAlreadySetAsUnique(uniqueObjects, object, options) {
+  return uniqueObjects.some((unique) => keyComparer(object, unique, options));
+}
+
+function getLatestInstance(objects, currentObject, options) {
+  const sortAttribute = options.duplicateConfig.timestampAttributeName;
+
+  const instances = objects.filter((object) =>
+    keyComparer(currentObject, object, options)
+  );
+
+  if (instances.length === 0) return instances[0];
+
+  const latest = instances.sort((a, b) =>
+    b[sortAttribute] >= a[sortAttribute] ? 1 : 0
+  );
+
+  return latest[0];
+}
+
+function filterUniqueObjects(objects = [], options = defaultOptions) {
+  if (
+    !options.duplicateConfig.partitionKeyAttributeName ||
+    !options.duplicateConfig.sortKeyAttributeName
+  ) {
+    throw new Error('Key attribute names must be set in duplicateConfig');
+  }
+
+  if (!objects.length) return objects;
+
+  const uniqueObjects = [];
+
+  objects.forEach((object) => {
+    if (objectAlreadySetAsUnique(uniqueObjects, object, options)) return;
+
+    const latest = getLatestInstance(objects, object, options);
+    uniqueObjects.push(latest);
+  });
+
+  return uniqueObjects;
+}
+
 module.exports = {
-  filterUniqueKeys
+  filterUniqueKeys,
+  filterUniqueObjects
 };
--- src/dynamodb/batch/duplicate-handling/filter.test.js
+++ src/dynamodb/batch/duplicate-handling/filter.test.js
@@ -10,13 +10,35 @@ describe('filter duplicates', () => {
     expect(uniqueKeys[0].id).toBe(1);
     expect(uniqueKeys[1].id).toBe(2);
   });
+
   it('should verify that keys for filtering are objects', () => {
     const keys = [{}, 'a'];
     expect(() => tested.filterUniqueKeys(keys)).toThrow(
       'All keys should must be objects'
     );
   });
-  it('should filter out duplicate objects by given root timestamp property', () => {});
+
+  it('should verify that key attribute names are set when filtering objects', () => {
+    expect(() => tested.filterUniqueObjects(testData)).toThrow(
+      'Key attribute names must be set in duplicateConfig'
+    );
+  });
+
+  it('should filter out duplicate objects by given root timestamp property', () => {
+    const uniqueObjects = tested.filterUniqueObjects(testData, {
+      duplicateConfig: {
+        timestampAttributeName: 'timestamp',
+        partitionKeyAttributeName: 'id',
+        sortKeyAttributeName: 'id2'
+      }
+    });
+
+    expect(uniqueObjects.length).toBe(2);
+    expect(uniqueObjects[0].id).toBe(1);
+    expect(uniqueObjects[0].timestamp).toBe('2022-05-10T10:36:12+02:00');
+    expect(uniqueObjects[1].id).toBe(2);
+  });
+
   it('should filter out duplicate objects by given nested timestamp property', () => {});
   it('should filter out duplicate objects without timestamp property', () => {});
 });
