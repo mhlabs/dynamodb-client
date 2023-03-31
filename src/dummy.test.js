6389758db6541dfab90dc1501db73392262a53d1
Fredrik Ståhl <fredrik.stahl@mathem.se>
2023-03-31 15:21:22 +0200

    chore: remove dummy test

--- src/dummy.test.js
+++
@@ -1,6 +0,0 @@
-// this file will be removed later on, used just to verify that gh actions work
-import { expect, it } from "vitest";
-
-it("should be true", () => {
-  expect(true).toBeTruthy();
-});
